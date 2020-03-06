ff294f1ea36c020ed796a2416b11b2ded6c8ffc7
Fernanda Rocha <fernandaporocha@gmail.com>
2020-03-06 16:48:40 +0000

    Created Student Entity

---
+++ com/rocha/jdbc/Student.java
@@ -0,0 +1,22 @@
+package com.rocha.jdbc;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+
+}
